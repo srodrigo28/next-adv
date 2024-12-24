d23d7e13b602bfbe8cf1421d41c7ecaaf7a58e7a
Sebastião Rodrigol <rodrigoexer1@gmail.com>
2024-12-23 23:58:08 -0300

    updated-50%

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,46 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="flex flex-col">
+      <section className="flex">
+        <img src="./image/home.png" className="flex-1 relative" alt="" />
+        <div className="absolute md:top-72 top-20 ml-10 md:ml-10">
+          <h2 className="text-white md:text-6xl text-3xl font-bold md:mb-28">JUROS ABUSIVOS </h2>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          <p className="md:text-xl text-sm text-center  bg-yellow-600 p-2 rounded-xl font-semibold px-5">  Saiba se você está pagando juros abusivos ou taxas indevidas. </p>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </section>
+      <section className="flex">
+        <img src="./image/page2.png" className="flex-1" alt="" />
+      </section>
+      <section className="flex flex-col">
+        <header className="flex flex-col bg-yellow-600 w-screen h-[30vh]">
+          <h2 className="text-5xl font-bold ml-10 text-black mt-28">O que fazer?</h2>
+        </header>
+        <article className="flex flex-col md:flex-row gap-2 bg-zinc-900 w-screen h-[80vh]">
+          <article className="flex-1 md:p-8 flex md:flex-col bg-zinc-900">
+              <img className="self-center" src="image/icon-balanca.png" width={100} alt="" />
+              <hr className="mb-5 mt-5" />
+              <h2 className="text-white mb-5">1º Passo</h2>
+              <p className="text-white">Agenda uma consulta com nossos advogados especialistas e faça uma análise gratuita 
+                do seu contrato.</p>   
+
+          </article>
+          <article className="flex-1 md:p-8 flex md:flex-col bg-zinc-900">    
+              <img className="self-center" src="image/icone-money.png" width={100} alt="" />
+              <hr className="mb-5 mt-5" />
+              <h2 className="text-white mb-5">2º Passo</h2>
+              <p className="text-white">Agenda uma consulta com nossos advogados especialistas e faça uma análise gratuita 
+                do seu contrato.</p>
+          </article>
+          <article className="flex-1 md:p-8 flex md:flex-col bg-zinc-900">
+            <img className="self-center" src="image/icon-arrow.png" width={100} alt="" />
+              <hr className="mb-5 mt-5" />
+              <h2 className="text-white mb-5">3º Passo</h2>
+              <p className="text-white">Agenda uma consulta com nossos advogados especialistas e faça uma análise gratuita 
+                do seu contrato.</p>
+          </article>
+        </article>
+      </section>
     </div>
   );
 }
